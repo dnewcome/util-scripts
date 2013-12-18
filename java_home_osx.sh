55217bda21d64dbbbdd765aad94c356559ed5812
Dan Newcome <dan@dnuke.com>
2013-12-17 18:23:54 -0800

    making lock command writable, adding java env setup for osx

---
+++ java_home_osx.sh
@@ -0,0 +1,3 @@
+# set java environment variable on OSX
+# http://stackoverflow.com/questions/6588390/where-is-java-home-on-osx-lion-10-7-mountain-lion-10-8-or-mavericks-10-9+  (/)
+export JAVA_HOME=`/usr/libexec/java_home -v 1.7`
